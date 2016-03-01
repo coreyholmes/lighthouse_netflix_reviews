4b7df89a24e193354821806e5d45116c5befa1a0
coreyholmes <coreyholmes20@gmail.com>
2016-03-01 01:40:50 +0000

    added form partials

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,75 @@
+class MoviesController < ApplicationController
+  
+  before_action :set_movie, except: [:index, :new, :create]
+
+  def index # GET
+    @movies = Movie.all
+  end
+
+  # --------------------------------------
+  # Create
+  # --------------------------------------
+
+  def new # GET
+    @movie = Movie.new
+  end
+
+  def create # POST
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path
+    else
+      erb :new
+    end
+  end
+
+  # --------------------------------------
+  # Read
+  # --------------------------------------
+
+  def show # GET
+
+  end
+
+  # --------------------------------------
+  # Update
+  # --------------------------------------
+  
+  def edit #GET
+
+  end
+
+  def update # PUT / PATCH
+    if @movie.update_attributes(movie_params)
+      redirect_to movies_path
+    else
+      erb :edit
+    end
+  end
+
+  # --------------------------------------
+  # Delete
+  # --------------------------------------
+  
+  def destroy # DELETE
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  # --------------------------------------
+
+   protected
+
+  def movie_params
+    params.require(:movie).permit(
+      :title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description
+    )
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+
+end
